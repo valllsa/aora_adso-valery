50dc80662cab7cc5cf38b770161fbec825b26239
Aprendiz <Aprendiz@BOGDFPCGMF1047.FormacionSena.RED>
2024-08-03 11:34:00 -0500

    Segundo cambio de actividad

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,27 @@
- import { StyleSheet, Text, View } from 'react-native'
- import React from 'react'
- import { Slot } from 'expo-router'
- 
- const RootLayout = () => {
-   return <Slot />
- }
- 
- export default RootLayout
+import React from 'react'; 
+import { StyleSheet, Text, View} from 'react-native'; 
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Text>Aora ADSO!</Text>
+    <StatusBar style='auto' />
+    <Link href="/profile" style={{color: 'blue'}}>Gon to Profile</Link>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}
+)
  
- 
+
+
--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-react'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@/components': './components',
-            '@/assets': './assets',
-          },
-        },
-      ],
-    ],
+    presets: ['babel-present-expo'],
+    plugins: ["nativewind/babel"]
   };
 };
 
